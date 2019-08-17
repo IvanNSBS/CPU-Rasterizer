ecc414f238678fff6d4416313b61b18c5378412e
IvanNSBS <insbs@cin.ufpe.br>
2019-08-17 15:29:39 -0300

    Adding Cohen-Sutherland algorithm Code Refactoring

--- sdl2-update/camera.h
+++ sdl2-update/camera.h
@@ -5,6 +5,11 @@
 #include "vec2.h"
 #include "matrix44.h"
 
+#ifdef _WIN32 || WIN32
+	#include <SDL.h>
+#elif defined(__unix__)
+	#include <SDL2/SDL.h>
+#endif
 
 class camera
 {
@@ -69,13 +74,6 @@ public:
         worldToCamera = camToWorld.inverse();
     }
 
-	void move(vec3 dir) {
-        camToWorld.mult_vec_matrix(dir, dir);
-		_from += dir;
-		_at += dir;
-		set_axis_and_matrix( _from, _at, _up, false);
-	}
-
     bool compute_pixel_coordinates(const vec3 &pWorld, vec2 &pRaster) 
     { 
 		vec3 ray = pWorld - _from;
@@ -113,73 +111,220 @@ public:
         set_axis_and_matrix(_from, _at, _up, true);
     }
 
-    void rot_x(float deg) {
+    void move(vec3 dir) {
+        camToWorld.mult_vec_matrix(dir, dir);
+		_from += dir;
+		_at += dir;
+		set_axis_and_matrix( _from, _at, _up, false);
+	}
 
-        float sen = sin(deg*3.14 / 180.0f);
-        float co = cos(deg*3.14 / 180.0f);
-        matrix44 rot(1, 0, 0, 0,
-                    0, co, -sen, 0,
-                    0, sen, co, 0,
-                    0, 0, 0, 1);
+    bool clip_line( vec2 &p0, vec2 &p1)
+    {
+        int t = 0, b = HEIGHT;
+        int l = 0, r = WIDTH;
+        int codep0 = 0000, codep1 = 0000;
+        
+        auto get_outcode = [&t, &l, &b, &r]( int &code, const vec2 &p){
+            code = INSIDE;          // initialised as being inside of [[clip window]]
 
-        matrix44 tr(		1,			  0,			0,			0,
-                            0,			  1,			0,			0,
-                            0,			  0,			1,			0,
-                    -_from.x(), -_from.y(), -_from.z(), 1);
-        matrix44 itr = tr.inverse();
-        matrix44 result = (tr*rot)*itr;
-        result.mult_point_matrix(_at,_at);
-        result.mult_vec_matrix(_up,_up);
-        _up.make_unit_vector();
+            if (p.x() < 0)           // to the left of clip window
+                code |= LEFT;
+            else if (p.x() > WIDTH)      // to the right of clip window
+                code |= RIGHT;
+            if (p.y() < 0)           // below the clip window
+                code |= BOTTOM;
+            else if (p.y() > HEIGHT)      // above the clip window
+                code |= TOP;
+        };
 
-		set_axis_and_matrix( _from, _at, _up, true);
+        get_outcode(codep0, p0);
+        get_outcode(codep1, p1);
 
+        bool accept = false;
+        unsigned int changed = 0;
+        while(true){
+            if( !(codep0 | codep1) ){
+                accept = true;
+                return changed > 0; // trivialmente aceito
+            }
+            else if(codep0 & codep1) // trivialmente recusado
+                return false;
+            else{
+                // failed both tests, so calculate the line segment to clip
+                // from an outside point to an intersection with clip edge
+                float x, y;
+                changed++;
+                // At least one endpoint is outside the clip rectangle; pick it.
+                unsigned char codeOut = codep0 ? codep0 : codep1;
 
+                // Now find the intersection point;
+                // use formulas:
+                //   slope = (p1[1] - p0[1]) / (x1 - x0)
+                //   x = x0 + (1 / slope) * (ym - p0[1]), where ym is 0 or HEIGHT
+                //   y = p0[1] + slope * (xm - x0), where xm is xmin or WIDTH
+                // No need to worry about divide-by-zero because, in each case, the
+                // outcode bit being tested guarantees the denominator is non-zero
+                if (codeOut & TOP) {           // point is above the clip window
+                    x = p0[0] + (p1[0] - p0[0]) * (HEIGHT - p0[1]) / (p1[1] - p0[1]);
+                    y = HEIGHT;
+                } else if (codeOut & BOTTOM) { // point is below the clip window
+                    x = p0[0] + (p1[0] - p0[0]) * (0 - p0[1]) / (p1[1] - p0[1]);
+                    y = 0;
+                } else if (codeOut & RIGHT) {  // point is to the right of clip window
+                    y = p0[1] + (p1[1] - p0[1]) * (WIDTH - p0[0]) / (p1[0] - p0[0]);
+                    x = WIDTH;
+                } else if (codeOut & LEFT) {   // point is to the left of clip window
+                    y = p0[1] + (p1[1] - p0[1]) * (0 - p0[0]) / (p1[0] - p0[0]);
+                    x = 0;
+                }
+
+                // Now we move outside point to intersection point to clip
+                // and get ready for next pass.
+                if (codeOut == codep0) {
+                    p0[0] = x;
+                    p0[1] = y;
+                    get_outcode(codep0, p0);
+                } else {
+                    p1[0] = x;
+                    p1[1] = y;
+                    get_outcode(codep1, p1);
+                }
+            }
+        }
+        return accept;
     }
 
-    void rot_y(float deg) {
+    void draw_lines( const vec2 &p0, const vec2 &p1, SDL_Renderer *renderer, const vec3 &col)
+    {
 
-        float sen = sin(deg*3.14 / 180.0f);
-        float co = cos(deg*3.14 / 180.0f);
-        matrix44 rot(co, 0, sen, 0,
-                    0, 1, 0, 0,
-                    -sen, 0, co, 0,
-                    0, 0, 0, 1);
+        // calculate dx , dy
+        int dx = p1.x() - p0.x();
+        int dy = p1.y() - p0.y();
 
-        matrix44 tr(		1,			  0,			0,			0,
-                            0,			  1,			0,			0,
-                            0,			  0,			1,			0,
-                    -_from.x(), -_from.y(), -_from.z(), 1);
-        matrix44 itr = tr.inverse();
+        // Depending upon absolute value of dx & dy
+        // choose number of steps to put pixel as
+        // steps = abs(dx) > abs(dy) ? abs(dx) : abs(dy)
+        int steps = abs(dx) > abs(dy) ? abs(dx) : abs(dy);
 
-        matrix44 result = (tr*rot)*itr;
-        result.mult_point_matrix(_at,_at);
-        result.mult_vec_matrix(_up,_up);
-        _up.make_unit_vector();
+        // calculate increment in x & y for each steps
+        float Xinc = dx / (float) steps;
+        float Yinc = dy / (float) steps;
 
-		set_axis_and_matrix( _from, _at, _up, true);
+        // Put pixel for each step
+        float X = p0.x();
+        float Y = p0.y();
+        for (int i = 0; i <= steps; i++)
+        {
+            SDL_RenderDrawPoint(renderer, X, Y);
+            X += Xinc;
+            Y += Yinc;
+        }
 
+        // int dx = p1.x() - p0.x();
+        // int dy = p1.y() - p0.y();
+        // // points are too close, draw point and return
+        // if( dx == 0 && dy == 0){
+        // 	SDL_RenderDrawPoint(renderer, p1.x(), p1.y());
+        // 	return;
+        // }
+
+        // vec2 origin = p0;
+        // vec2 line = unit_vector(p1 - p0);
+        // float end, start, step;
+        // if( dx != 0 ){
+        // 	end = p1.x() > p0.x() ? p1.x() : p0.x();
+        // 	start = p1.x() > p0.x() ? p0.x() : p1.x();
+        // 	step = abs(line.x());
+        // }
+        // else{
+        // 	end = p1.y() > p0.y() ? p1.y() : p0.y();
+        // 	start = p1.y() > p0.y() ? p0.y() : p1.y();
+        // 	step = abs(line.y());	
+        // }
+
+        // // printf("start = %f | end = %f | step = %f\n", start, end, step);
+
+        // unsigned char a = 0000;
+        // for(float i = start; i <= end; i+=step){
+        // 	SDL_RenderDrawPoint(renderer, origin.x(), origin.y());
+        // 	origin += line;
+        // }
     }
 
-    void rot_z(float deg) {
+    void render_scene( std::vector<Obj> objs, SDL_Window *wind, SDL_Renderer* renderer) {
 
-        matrix44 tr(1, 0, 0, 0,
-                    0, 1, 0, 0,
-                    0, 0, 1, 0,
-            -_from.x(), -_from.y(), -_from.z(), 1);
-        matrix44 itr = tr.inverse();
-        float sen = sin(deg*3.14 / 180.0);
-        float co = cos(deg*3.14 / 180);
-        matrix44 rot(co, -sen, 0, 0,
-                     sen, co, 0, 0,
-                     0, 0, 1, 0,
-                     0, 0, 0, 1);
-        matrix44 result = (tr*rot)*itr;
-        result.mult_point_matrix(_at,_at);
-        result.mult_vec_matrix(_up,_up);
-        set_axis_and_matrix( _from, _at, _up, true);
-        rotation[2] += deg;
+        // for (auto obj : objs){
+        // 	std::sort(obj.mesh.tris.begin(), obj.mesh.tris.end(), [](Triangle& t1, Triangle &t2)
+        // 		{
+        // 			std::vector<float> d1 = { t1.vert[0].z(), t1.vert[1].z(), t1.vert[2].z() };
+        // 			std::vector<float> d2 = { t2.vert[0].z(), t2.vert[1].z(), t2.vert[2].z() };
+
+        // 			std::sort( d1.begin(), d1.end(), []( float &f1, float &f2) { return f1 > f2; } );
+        // 			std::sort( d2.begin(), d2.end(), []( float &f1, float &f2) { return f1 > f2; } );
+
+        // 			if( d1[0] != d2[0])
+        // 				return d1[0] < d2[0];
+        // 			else if (d1[1] != d2[1])
+        // 				return d1[1] < d2[2];
+        // 			else
+        // 				return d1[2] < d2[2];
+        // 		});
+        // }
+
+        vec3 light(0.0f, 0.0f, -1.0f);
+        light.make_unit_vector();
+
+        for (auto obj : objs){
+            for (int i = 0; i < obj.mesh.tris.size(); i++)
+            {
+                vec3 n1 = obj.mesh.tris[i].vertex[0].normal;
+                vec3 n2 = obj.mesh.tris[i].vertex[1].normal;
+                vec3 n3 = obj.mesh.tris[i].vertex[2].normal;
+                vec3 normal = (1.f/3.f) * n1 + (1.f/3.f)*n2 + (1.f/3.f)*n3;
+
+                vec3 ray = obj.mesh.tris[i].vertex[0].pos - _from;
+                if ( dot(normal, ray ) <= 0.0f ) {
+
+                    float dp = dot(normal, light);
+                    vec2 praster1;
+                    vec2 praster2;
+                    vec2 praster3;
+
+                    vec3 col(255, 255, 255);
+                    SDL_SetRenderDrawColor(renderer, obj.col.x(), obj.col.y(), obj.col.z(), SDL_ALPHA_OPAQUE);
+
+                    bool v1, v2, v3;
+                    v1 = compute_pixel_coordinates(obj.mesh.tris[i].vertex[0].pos, praster1);
+                    v2 = compute_pixel_coordinates(obj.mesh.tris[i].vertex[1].pos, praster2);
+                    v3 = compute_pixel_coordinates(obj.mesh.tris[i].vertex[2].pos, praster3);
+
+
+                    if(v1 && v2){
+                        bool clip = clip_line(praster1, praster2);
+                        if(clip)
+                            SDL_SetRenderDrawColor(renderer, 255, 0, 0, SDL_ALPHA_OPAQUE);
+                        draw_lines(praster1, praster2, renderer, col);
+                        // SDL_RenderDrawLine(renderer, praster1[0], praster1[1], praster2[0], praster2[1]);
+                    }
+                    if(v1 && v3){
+                        bool clip = clip_line(praster1, praster3);
+                        if(clip)
+                            SDL_SetRenderDrawColor(renderer, 255, 0, 0, SDL_ALPHA_OPAQUE);
+                        // SDL_RenderDrawLine(renderer, praster1[0], praster1[1], praster3[0], praster3[1]);
+                        draw_lines(praster1, praster3, renderer, col);
+                    }
+                    if(v2 && v3){
+                        bool clip = clip_line(praster2, praster3);
+                        if(clip)
+                            SDL_SetRenderDrawColor(renderer, 255, 0, 0, SDL_ALPHA_OPAQUE);
+                        // SDL_RenderDrawLine(renderer, praster2[0], praster2[1], praster3[0], praster3[1]);
+                        draw_lines(praster2, praster3, renderer, col);
+                    }
+                }
+            }
+        }
     }
+
 };
 
 
--- sdl2-update/main.cpp
+++ sdl2-update/main.cpp
@@ -8,18 +8,17 @@
 #include "ImGUI/imgui_sdl.h"
 #include "ImGUI/imgui.h"
 
-#ifdef _WIN32 || WIN32
-	#include <SDL.h>
-#elif defined(__unix__)
-	#include <SDL2/SDL.h>
-#endif
-
 #define MIN 0
 #define MAX 1
 #define PI 3.14159265
 
-static const int WIDTH = 800;
-static const int HEIGHT = 600;
+static const int WIDTH = 400;
+static const int HEIGHT = 200;
+static const int INSIDE = 0; // 0000
+static const int LEFT = 1;   // 0001
+static const int RIGHT = 2;  // 0010
+static const int BOTTOM = 4; // 0100
+static const int TOP = 8;    // 1000
 
 bool intersects_triangle( Triangle &tr, vec3 ray_org, vec3 ray_dir, vec3 &out_col, vec3 &out_point, float &t)
 {
@@ -60,133 +59,6 @@ bool intersects_triangle( Triangle &tr, vec3 ray_org, vec3 ray_dir, vec3 &out_co
 	}// This means that there is a line intersection but not a ray intersection.
 }
 
-void fill_triangle( const Triangle &tr, SDL_Renderer *renderer, const vec3 &col)
-{
-
-}
-
-void draw_lines( const vec2 &p0, const vec2 &p1, SDL_Renderer *renderer, const vec3 &col)
-{
-
-	// // calculate dx , dy
-	// int dx = p1.x() - p0.x();
-	// int dy = p1.y() - p0.y();
-
-	// // Depending upon absolute value of dx & dy
-	// // choose number of steps to put pixel as
-	// // steps = abs(dx) > abs(dy) ? abs(dx) : abs(dy)
-	// int steps = abs(dx) > abs(dy) ? abs(dx) : abs(dy);
-
-	// // calculate increment in x & y for each steps
-	// float Xinc = dx / (float) steps;
-	// float Yinc = dy / (float) steps;
-
-	// // Put pixel for each step
-	// float X = p0.x();
-	// float Y = p0.y();
-	// for (int i = 0; i <= steps; i++)
-	// {
-	// 	SDL_RenderDrawPoint(renderer, X, Y);
-	// 	X += Xinc;
-	// 	Y += Yinc;
-	// }
-
-	int dx = p1.x() - p0.x();
-	int dy = p1.y() - p0.y();
-	// points are too close, draw point and return
-	if( dx == 0 && dy == 0){
-		SDL_RenderDrawPoint(renderer, p1.x(), p1.y());
-		return;
-	}
-
-	vec2 origin = p0;
-	vec2 line = unit_vector(p1 - p0);
-	float end, start, step;
-	if( dx != 0 ){
-		end = p1.x() > p0.x() ? p1.x() : p0.x();
-		start = p1.x() > p0.x() ? p0.x() : p1.x();
-		step = abs(line.x());
-	}
-	else{
-		end = p1.y() > p0.y() ? p1.y() : p0.y();
-		start = p1.y() > p0.y() ? p0.y() : p1.y();
-		step = abs(line.y());	
-	}
-
-	// printf("start = %f | end = %f | step = %f\n", start, end, step);
-
-	for(float i = start; i <= end; i+=step){
-		SDL_RenderDrawPoint(renderer, origin.x(), origin.y());
-		origin += line;
-	}
-}
-
-void render_scene( std::vector<Obj> objs, camera &cam, SDL_Window *wind, SDL_Renderer* renderer) {
-
-	// for (auto obj : objs){
-	// 	std::sort(obj.mesh.tris.begin(), obj.mesh.tris.end(), [](Triangle& t1, Triangle &t2)
-	// 		{
-	// 			std::vector<float> d1 = { t1.vert[0].z(), t1.vert[1].z(), t1.vert[2].z() };
-	// 			std::vector<float> d2 = { t2.vert[0].z(), t2.vert[1].z(), t2.vert[2].z() };
-
-	// 			std::sort( d1.begin(), d1.end(), []( float &f1, float &f2) { return f1 > f2; } );
-	// 			std::sort( d2.begin(), d2.end(), []( float &f1, float &f2) { return f1 > f2; } );
-
-	// 			if( d1[0] != d2[0])
-	// 				return d1[0] < d2[0];
-	// 			else if (d1[1] != d2[1])
-	// 				return d1[1] < d2[2];
-	// 			else
-	// 				return d1[2] < d2[2];
-	// 		});
-	// }
-
-	vec3 light(0.0f, 0.0f, -1.0f);
-	light.make_unit_vector();
-
-	for (auto obj : objs){
-		for (int i = 0; i < obj.mesh.tris.size(); i++)
-		{
-			vec3 n1 = obj.mesh.tris[i].vertex[0].normal;
-			vec3 n2 = obj.mesh.tris[i].vertex[1].normal;
-			vec3 n3 = obj.mesh.tris[i].vertex[2].normal;
-			vec3 normal = (1.f/3.f) * n1 + (1.f/3.f)*n2 + (1.f/3.f)*n3;
-
-			vec3 ray = obj.mesh.tris[i].vertex[0].pos - cam._from;
-			if ( dot(normal, ray ) < 0.0f ) {
-
-				float dp = dot(normal, light);
-				vec2 praster1;
-				vec2 praster2;
-				vec2 praster3;
-
-				vec3 col(255, 255, 255);
-				SDL_SetRenderDrawColor(renderer, obj.col.x(), obj.col.y(), obj.col.z(), SDL_ALPHA_OPAQUE);
-
-				bool v1, v2, v3;
-				v1 = cam.compute_pixel_coordinates(obj.mesh.tris[i].vertex[0].pos, praster1);
-				v2 = cam.compute_pixel_coordinates(obj.mesh.tris[i].vertex[1].pos, praster2);
-				v3 = cam.compute_pixel_coordinates(obj.mesh.tris[i].vertex[2].pos, praster3);
-
-				if(v1 && v2)
-					draw_lines(praster1, praster2, renderer, col);
-				if(v1 && v3)
-					draw_lines(praster1, praster3, renderer, col);
-				if(v2 && v3)
-					draw_lines(praster2, praster3, renderer, col);
-
-				// if(v1 && v2)
-				// 	SDL_RenderDrawLine(renderer, praster1[0], praster1[1], praster2[0], praster2[1]);
-				// if(v1 && v3)
-				// 	SDL_RenderDrawLine(renderer, praster1[0], praster1[1], praster3[0], praster3[1]);
-				// if(v2 && v3)
-				// 	SDL_RenderDrawLine(renderer, praster2[0], praster2[1], praster3[0], praster3[1]);
-			}
-		}
-	}
-}
-
-
 
 int main(int argc, char* argv[])
 {
@@ -202,9 +74,6 @@ int main(int argc, char* argv[])
             
 			std::vector<Obj> objects;
             objects.push_back( Obj("./monkey_smooth.obj") );
-			objects[0].rot_x(34.6);
-			objects[0].rot_y(-50.f);
-			objects[0].rot_z(-4.56f);
 
 			ImGui::CreateContext();
 			ImGuiSDL::Initialize(renderer, WIDTH, HEIGHT);
@@ -225,8 +94,6 @@ int main(int argc, char* argv[])
 				int mouseX, mouseY;
 				const int buttons = SDL_GetMouseState(&mouseX, &mouseY);
 
-				// Setup low-level inputs (e.g. on Win32, GetKeyboardState(), or write to those fields from your Windows message loop handlers, etc.)
-				
 				io.DeltaTime = 1.0f / 60.0f;
 				io.MousePos = ImVec2(static_cast<float>(mouseX), static_cast<float>(mouseY));
 				io.MouseDown[0] = buttons & SDL_BUTTON(SDL_BUTTON_LEFT);
@@ -234,32 +101,9 @@ int main(int argc, char* argv[])
 
 
 				ImGui::NewFrame();
-			
-				//ImGui::ShowDemoWindow();
-
-				// Create a window called "My First Tool", with a menu bar.
-				ImGui::Begin("My First Tool", &my_tool_active, ImGuiWindowFlags_MenuBar);
-				if (ImGui::BeginMenuBar())
-				{
-					if (ImGui::BeginMenu("File"))
-					{
-						if (ImGui::MenuItem("Open..", "Ctrl+O")) { objects.push_back( Obj("./monkey_smooth.obj") ); objects[objects.size()-1].translate( vec3(10, 0, 0) ); }
-						if (ImGui::MenuItem("Save", "Ctrl+S"))   { /* Do stuff */ }
-						if (ImGui::MenuItem("Close", "Ctrl+W"))  { my_tool_active = false; }
-						ImGui::EndMenu();
-					}
-					ImGui::EndMenuBar();
-				}
-
 				// Edit a color (stored as ~4 floats)
 				ImGui::ColorEdit4("Color", my_color);
-
-				// Plot some values
-				const float my_values[] = { 0.2f, 0.1f, 1.0f, 0.5f, 0.9f, 2.2f };
-				ImGui::PlotLines("Frame Times", my_values, IM_ARRAYSIZE(my_values));
-
 				// Display contents in a scrolling region
-				ImGui::TextColored(ImVec4(1,1,0,1), "Important Stuff");
 				ImGui::Text("FPS: %d", (int)ms);
 				ImGui::BeginChild("Scrolling");
 				ImGui::Text("Cam Rotation: (%f, %f, %f)", cam.rotation.x(), cam.rotation.y(), cam.rotation.z());
@@ -270,9 +114,7 @@ int main(int argc, char* argv[])
 
 				SDL_SetRenderDrawColor(renderer, my_color[0]*255, my_color[1]*255, my_color[2]*255, my_color[3]*255);
 				SDL_RenderClear(renderer);
-
-
-                render_scene(objects, cam, window, renderer);
+                cam.render_scene(objects, window, renderer);
 				ImGui::Render();
 				ImGuiSDL::Render(ImGui::GetDrawData());
                 SDL_RenderPresent(renderer);
@@ -302,17 +144,6 @@ int main(int argc, char* argv[])
 							cam.move( vec3(0.00f, -up_speed, 0.0f) );
 						if( event.key.keysym.sym == SDLK_e )
 							cam.move( vec3(0.0f, up_speed, 0.0f) );
-
-						
-						if( event.key.keysym.sym == SDLK_z )
-							cam.rot_x(450.f * ms);
-						if( event.key.keysym.sym == SDLK_x )
-							cam.rot_y(450.f * ms);
-
-						if( event.key.keysym.sym == SDLK_c )
-							cam.rot_x(-450.f * ms);
-						if( event.key.keysym.sym == SDLK_v )
-							cam.rot_y(-450.f * ms);
 					}
 
                     if( event.type == SDL_MOUSEBUTTONDOWN )
